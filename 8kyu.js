b17d0c72e26f7f7c43fd3252927637b61cb5f1ae
hamidullo <ahamidullo248@gmail.com>
2025-12-17 22:07:30 +0500

    2problems solved(8kyu)

--- 8kyu.js
+++ 8kyu.js
@@ -1,1 +1,23 @@
 
+///////////////  8kyu  /////////////
+
+// 1. Multiply
+
+// function multiply(a, b){
+//  return  a * b
+// }
+
+// console.log(multiply(1,2));
+// console.log(multiply(6,2));
+// console.log(multiply(2,2));
+
+
+// 2. Even or Odd
+
+// function evenOrOdd(number) {
+//   return number % 2 == 0 ? "Even":"Odd"
+// }
+
+// console.log(evenOrOdd(2));
+// console.log(evenOrOdd(7));
+// console.log(evenOrOdd(3));

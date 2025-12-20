22af9f1372ebbc5c4c7514bc5a74ddee79b1b3dd
hamidullo <ahamidullo248@gmail.com>
2025-12-20 14:05:05 +0500

    some solutions with 8kyu

--- 8kyu.js
+++ 8kyu.js
@@ -21,3 +21,52 @@
 // console.log(evenOrOdd(2));
 // console.log(evenOrOdd(7));
 // console.log(evenOrOdd(3));
+
+
+// 3. Convert number to String!
+
+// function numberToString(num) {
+//   // Return a string of the number here!
+//   return String(num)
+// }
+
+// console.log(numberToString(123));
+// console.log(numberToString(-123));
+// console.log(numberToString(323));
+
+
+// 4. Reversed Strings
+
+    /// solution 1 ///
+
+    // function solution(str){
+    //   let res =""
+    //   for(let i = str.length-1; i>=0; i--){
+    //     res += str[i]
+    //   }
+    //   return res
+    // }
+
+    // console.log(solution('hello'));
+    // console.log(solution(''));
+    // console.log(solution('h'));
+
+
+    /// solution 2 ///
+
+    // function solution(str){
+    //   return str.split("").reverse().join("")
+    // }
+
+    // console.log(solution('hello'));
+    // console.log(solution(''));
+    // console.log(solution('h'));
+
+
+
+
+// function makeNegative(num) {
+//   return num < 0 ? Math.abs(num) : num  - (num * 2)
+// }
+
+// console.log(makeNegative(9));

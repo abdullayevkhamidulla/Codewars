6c3058b9c3fa4bec741ebb52bdc852e173d0e9a2
hamidullo <ahamidullo248@gmail.com>
2025-12-22 16:13:25 +0500

    one 5kyu solution

---
+++ 5kyu.js
@@ -0,0 +1,71 @@
+
+// console.time("test");
+// function zero(callback, n=0) {
+//     let res = n + callback||0
+//     return Math.floor(eval(res))
+// }
+// function one(callback, n=1) {
+//     let res = n + callback||1
+//     return Math.floor(eval(res))
+// }
+// function two(callback, n=2) {
+//     let res = n + callback||2
+//     return Math.floor(eval(res))
+// }
+// function three(callback, n=3) {
+//     let res = n + callback||3
+//     return Math.floor(eval(res))
+// }
+// function four(callback,n=4) {
+//     let res = n + callback||4
+//     return Math.floor(eval(res))
+// }
+// function five(callback,n=5) {
+//     let res = n + callback||5
+//     return Math.floor(eval(res))
+// }
+// function six(callback , n=6) {
+//     let res = n + callback||6
+//     return Math.floor(eval(res))
+// }
+// function seven(callback , n=7) {
+//       let res = n + callback||7
+//     return Math.floor(eval(res))
+// }
+// function eight(callback, n=8) {
+//       let res = n + callback||8
+//     return Math.floor(eval(res))
+
+
+// }
+// function nine(callback, n=9) {
+//       let res = n + callback||9
+//     return Math.floor(eval(res))
+// }
+
+// function plus(callback,res = '+') {
+//     return res + callback
+// }
+// function minus(callback,res = '-') {
+//     return res + callback
+// }
+
+// function times(callback ,res = '*') {
+//     return res + callback
+// }
+
+// function dividedBy(callback,res = '/') {
+//     return res + callback
+// }
+
+// console.log(seven(times(five())));
+// console.log(four(plus(nine())));
+// console.log(seven(dividedBy(three())));
+// console.log(six(dividedBy(two())));
+
+// console.timeEnd("test");
+
+
+
+
+

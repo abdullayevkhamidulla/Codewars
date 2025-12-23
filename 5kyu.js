4ffb6d40f66e0f336ae033be28b88e93d45598b5
hamidullo <ahamidullo248@gmail.com>
2025-12-23 11:08:20 +0500

    one more 5kyu

--- 5kyu.js
+++ 5kyu.js
@@ -66,6 +66,14 @@
 // console.timeEnd("test");
 
 
+// function domainName(url){
+//   return url.replace("http://", "").replace("https://","").replace("www.", "").split(".")[0]
+// }
+
+// console.log(domainName("http://github.com/carbonfive/raygun"));// github
+// console.log(domainName("http://www.zombie-bites.com"));//zombie-bites
+// console.log(domainName("https://www.cnet.com"));// cnet
+
 
 
 
